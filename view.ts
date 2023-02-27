afc021206eba8ffe047b572e30cee8eca89a4d06
Marcus Moore <contact@marcusmoore.io>
2023-02-26 17:33:39 -0800

    More hacking

--- view.ts
+++ view.ts
@@ -1,35 +1,47 @@
-import { ItemView, WorkspaceLeaf } from "obsidian";
+import {ItemView, WorkspaceLeaf} from "obsidian";
+import {getAPI} from "obsidian-dataview";
+
+const api = getAPI();
 
 export const VIEW_SIDEBAR_TODOS = "example-view";
 
 export class TodoView extends ItemView {
-  constructor(leaf: WorkspaceLeaf) {
-    super(leaf);
-  }
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
 
-  getViewType() {
-    return VIEW_SIDEBAR_TODOS;
-  }
+	getViewType() {
+		return VIEW_SIDEBAR_TODOS;
+	}
 
-  getDisplayText() {
-    return "Example view";
-  }
+	getDisplayText() {
+		return "Example view";
+	}
 
-  async onOpen() {
-    const container = this.containerEl.children[1];
-    container.empty();
-    // container.createEl("h4", { text: "Hello there...I'm over here..." });
-    
-    const rootEl = document.createElement('div');
+	async onOpen() {
 
-    rootEl.createDiv()
-      .createSpan({cls: 'title'})
-      .setText("Oh hi...");
+		// const page = api.page('Untitled.md');
+		//
+		// console.log(page);
+		// console.log(page.file);
+		this.writeDefaultText();
+	}
 
-    container.appendChild(rootEl);
-  }
+	private writeDefaultText() {
+		const container = this.containerEl.children[1];
+		container.empty();
+		container.createEl("h4", {text: "Hello there...I'm over here..."});
 
-  async onClose() {
-    // Nothing to clean up.
-  }
+		const rootEl = document.createElement('div');
+
+		rootEl.createDiv()
+			.createSpan({cls: 'title'})
+			.setText("Oh hi...");
+
+		container.appendChild(rootEl);
+	}
+
+	async onClose() {
+		// Nothing to clean up.
+	}
 }
