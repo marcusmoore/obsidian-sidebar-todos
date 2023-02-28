1ea00893bfeddb5acc307795d3f4512873d6066b
Marcus Moore <contact@marcusmoore.io>
2023-02-27 23:04:44 -0800

    Remove settings code

--- main.ts
+++ main.ts
@@ -1,22 +1,11 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {App, Editor, MarkdownView, Modal, Plugin} from 'obsidian';
 import {VIEW_SIDEBAR_TODOS, TodoView} from './view';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
-
 		this.registerView(
 			VIEW_SIDEBAR_TODOS,
 			(leaf) => new TodoView(leaf)
@@ -71,9 +60,6 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
 		this.registerEvent(
 			this.app.vault.on('modify', (file) => {
 				if (file.path === this.app.workspace.getActiveFile()?.path) {
@@ -92,14 +78,6 @@ export default class MyPlugin extends Plugin {
 
 	onunload() {
 		this.app.workspace.detachLeavesOfType(VIEW_SIDEBAR_TODOS);
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
 	}
 
 	async activateView(active = true) {
@@ -129,34 +107,5 @@ class SampleModal extends Modal {
 	onClose() {
 		const {contentEl} = this;
 		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
